a9ae14e355ac84d7c1f0fc687e987a2d77a15f12
Harshilwalia777 <127544480+Harshilwalia777@users.noreply.github.com>
2024-10-30 23:43:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector('.wrapper');
+const btnPopup=document.querySelector('.btn-Login');
+const iconClose=document.querySelector('.icon-close');
+const registerLink= document.querySelector('.register-link')
+const loginLink=document.querySelector('.login-link')
+btnPopup.addEventListener('click', ()=> {
+ wrapper.classList.add('active-popup');
+});
+iconClose.addEventListener('click', ()=> {
+ wrapper.classList.remove('active-popup');
+});
+registerLink.addEventListener('click',()=> {
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener('click',()=> {
+    wrapper.classList.remove('active');
+});
